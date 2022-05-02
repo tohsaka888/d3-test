a5aa577a03b01f63e42350a44ef3ac28599307e8
tohsaka888 <156132264@qq.com>
2022-05-02 09:37:37 +0800

    feat: test bar graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+import * as d3 from "d3";
+
+const DUMMY_DATA = [
+  { id: "d1", value: 10, region: "China" },
+  { id: "d2", value: 20, region: "China" },
+  { id: "d3", value: 30, region: "India" },
+  { id: "d4", value: 40, region: "India" },
+  { id: "d5", value: 50, region: "US" },
+];
 
 function App() {
+  const containerRef = useRef<HTMLDivElement>();
+  useEffect(() => {
+    const container = d3.select(".container").style("border", "1px solid red");
+    const bars = container
+      .selectAll(".bar")
+      .data(DUMMY_DATA)
+      .enter()
+      .append("div")
+      .classed("bar", true)
+      .style("width", 50 + 'px')
+      .style("height", (d) => d.value * 10 + 'px');
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className="container"
+        ref={(ref) => {
+          if (ref) {
+            containerRef.current = ref;
+          }
+        }}
+      >
+
+      </div>
     </div>
   );
 }
